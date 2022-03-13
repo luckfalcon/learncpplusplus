50aa72ab7c8e1a2715e5d011bbfdbf8c16f43738
luckfalcon <1534599943@qq.com>
2022-03-13 18:53:05 +0800

    push cplusplus file to master

---
+++ ctest/ctest/calculator.h
@@ -0,0 +1,16 @@
+#ifndef _CLAC_
+#define _CLAC_
+#include<ctype.h>
+
+void push(double f);
+double pop(void);
+int getop(char s[]);
+
+
+
+
+
+
+
+
+#endif
---
+++ ctest/ctest/meme.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int *p = (int)malloc(4 * sizeof(int));
+//memset();
